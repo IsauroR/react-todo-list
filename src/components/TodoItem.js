55349d8fb7df2aeb0601744f1fce0bdf008497d2
IsauroR <isauroruvalcaba@sbcglobal.net>
2019-02-17 15:14:11 -0800

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class TodoItem extends Component {
+  render() {
+    const { title, handleDelete /*itemId*/, handleEdit } = this.props;
+    // console.log(title)
+    return (
+      <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+        <h6>{title}</h6>
+        <div className="todo-icon">
+          <span className="mx-2 text-success" onClick={handleEdit}>
+            <i className="fas fa-pen" />
+          </span>
+          <span className="mx-2 text-danger" onClick={handleDelete}>
+            {/* span className="mx-2 text-danger" onClick={() => handleDelete(itemId)}> */}
+            <i className="fas fa-trash" />
+          </span>
+        </div>
+      </li>
+    );
+  }
+}
